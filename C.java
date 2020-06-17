a558876b3038df642c0eb7f41101859659682033
ghkddlr422 <66246456+ghkddlr422@users.noreply.github.com>
2020-06-17 17:58:42 +0900

    Add files via upload

---
+++ C.java
@@ -0,0 +1,9 @@
+package Ch001;
+import sec12.exam01_package_compile.*;
+
+public class C {
+//필드
+	A a1 = new A(true);
+//	A a2 = new A(1);					//default 생성자 접근 불가(컴파일 에러)
+//	A a3 = new A ("문자열");				//private 생성자 접근 불가(컴파일 에러)
+}
---
+++ PersonExample.java
@@ -0,0 +1,10 @@
+package Ch001;
+
+public class PersonExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
