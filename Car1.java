ae76c53fa3f18afd084e7cbfe9a28afe86683c1c
ghkddlr422 <66246456+ghkddlr422@users.noreply.github.com>
2020-06-18 17:13:49 +0900

    Add files via upload

---
+++ Car1.java
@@ -0,0 +1,27 @@
+package Ch001;
+
+public class Car1 {
+//필드
+	private int speed;
+	private boolean stop;
+	
+	//생성자
+	
+	//메소드
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		if(speed<0) {
+		}else {
+			
+		}this.speed = speed;
+	}
+	public boolean isStop() {
+		return stop;
+	}
+	public void setStop(boolean stop) {
+		this.stop = stop;
+		this.speed=0;
+	}
+}
